4e0e621a523402903e8f63faceaaac4349d296ef
SadnessFM <oldschool_otaku@proton.me>
2023-04-03 14:59:52 +0300

    ПОШЕЛ НАХУЙ

--- ZikaZika/Client/Services/ProductService/ProductService.cs
+++ ZikaZika/Client/Services/ProductService/ProductService.cs
@@ -1,7 +1,4 @@
-﻿using System.Net.Http;
-using System.Net.Http.Json;
-using System.Text;
-using System.Text.Json;
+﻿using System.Net.Http.Json;
 using ZikaZika.Shared;
 
 namespace ZikaZika.Client.Services.ProductService;
@@ -10,7 +7,7 @@ public class ProductService : IProductService
 {
     private readonly HttpClient _http;
 
-    public event Action OnChange;
+    public event Action OnChange = null!;
 
     public List<Product> Products { get; set; } = new();
 
@@ -23,26 +20,33 @@ public class ProductService : IProductService
     {
         if (categoryUrl == null)
         {
-            Products = await _http.GetFromJsonAsync<List<Product>>("api/Product");
+            Products = await _http.GetFromJsonAsync<List<Product>>("api/Product") ?? throw new InvalidOperationException();
         }
         else
         {
-            Products = await _http.GetFromJsonAsync<List<Product>>($"api/Product/Category/{categoryUrl}");
+            Products = await _http.GetFromJsonAsync<List<Product>>($"api/Product/Category/{categoryUrl}") ?? throw new InvalidOperationException();
         }
         OnChange.Invoke();
     }
 
     public async Task<Product> GetProduct(int id)
     {
-        return await _http.GetFromJsonAsync<Product>($"api/Product/{id}");
+        return await _http.GetFromJsonAsync<Product>($"api/Product/{id}") ?? throw new InvalidOperationException();
     }
 
     public async Task<List<Product>> SearchProducts(string searchText)
     {
-        return await _http.GetFromJsonAsync<List<Product>>($"api/Product/Search/{searchText}");
+        return await _http.GetFromJsonAsync<List<Product>>($"api/Product/Search/{searchText}") ?? throw new InvalidOperationException();
     }
 
-    public async Task<Product> AddProduct(Product product)
+    public async Task<HttpResponseMessage> AddProduct(Product product)
+    {
+        return await _http.PostAsJsonAsync("api/ProductController/AddProduct/", product);
+    }
+}
+
+/*
+ public async Task<Product> AddProduct(Product product)
     {
         string jsonProduct = JsonSerializer.Serialize(product);
         StringContent content = new(jsonProduct, Encoding.UTF8, "application/json");
@@ -51,25 +55,4 @@ public class ProductService : IProductService
         string responseBody = await response.Content.ReadAsStringAsync();
         return JsonSerializer.Deserialize<Product>(responseBody) ?? throw new InvalidOperationException();
     }
-}
-
-/* XHRPOSThttps://localhost:44332/api/ProductController/AddProduct/ZikaZika.Shared.Product
-[HTTP/2 405 Method Not Allowed 4ms]
-
- 
-1
-
-{
-"Title":"fsdfsadf",
-"Description":"dvsdfvdf",
-"Image":"https://via.placeholder.com/300x300",
-"CategoryId":3,
-"DateCreated":"2023-04-03T12:40:11.415+03:00",
-"Views":0,
-"IsPublic":false,
-"IsDeleted":false,
-"DateUpdated":"2023-04-03T12:40:11.431+03:00"
-}
-
-
 */
--- ZikaZika/Shared/Product.cs
+++ ZikaZika/Shared/Product.cs
@@ -1,16 +1,12 @@
-﻿using System.Text.Json.Serialization;
-
-namespace ZikaZika.Shared;
+﻿namespace ZikaZika.Shared;
 
 public class Product
 {
-    [JsonIgnore]
     public int Id { get; set; }
     public string Title { get; set; }
     public string Description { get; set; }
     public string Image { get; set; } = "https://via.placeholder.com/300x300";
     public int CategoryId { get; set; }
-    [JsonIgnore]
     public List<ProductVariant> Variants { get; set; } = new();
     public DateTime? DateCreated { get; set; } = DateTime.Now;
     public int Views { get; set; }
